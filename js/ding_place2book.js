121ebee1b5d8c302646a38c0fb358caf038be77c
Tobias F. Petersen <tobsf@hotmail.com>
2017-11-08 12:19:22 +0100

    make it work for full_page view

--- js/ding_place2book.js
+++ js/ding_place2book.js
@@ -5,9 +5,19 @@
  */
 jQuery(document).ready(function($) {
 	$('.place2book-ticketinfo').each(function() { 
+            debugger;
 	    var obj = this;
+            if(Drupal.settings.ding_place2book.pre_loader_url != undefined)
+            {
+                $(obj).parent().append("<div class='p2b_preloader'><a target='_blank' href='"+Drupal.settings.ding_place2book.pre_loader_url+"'>Bestil billet(ter)</a><i class='fa fa-cog fa-spin fa-2x fa-fw'></i><div>");
+            }
+            else
+            {
+                $(obj).parent().append("<div class='p2b_preloader'><i class='fa fa-cog fa-spin fa-2x fa-fw'></i><div>");
+            }
 		$.getJSON(Drupal.settings.basePath + 'ding/place2book/ticketinfo/' + this.value, function(data) {
 		  $(obj).replaceWith(data['markup']);
+                  $('.p2b_preloader').remove();
 		});		
     });
 });
