1720672f08f85285fdac59591a23e245b0b8a2ab
dev <dev@dev-VirtualBox>
2018-10-23 14:09:57 +0200

    make request uniqe to avoid browser cache

--- js/ding_place2book.js
+++ js/ding_place2book.js
@@ -15,7 +15,7 @@ jQuery(document).ready(function($) {
             {
                 $(obj).parent().append("<div class='p2b_preloader'><i class='fa fa-cog fa-spin fa-2x fa-fw'></i><div>");
             }
-		$.getJSON(Drupal.settings.basePath + 'ding/place2book/ticketinfo/' + this.value, function(data) {
+		$.getJSON(Drupal.settings.basePath + 'ding/place2book/ticketinfo/' + this.value + "?time=" + new Date().getTime(), function(data) {
 		  $(obj).replaceWith(data['markup']);
                   $('.p2b_preloader').remove();
 		});		
